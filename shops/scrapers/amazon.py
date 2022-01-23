87f16babafd09e330240a91ed4abf7a260e001e7
Paaksing <paaksingtech@gmail.com>
2022-01-23 19:33:52 -0400

    Fix: Amazon changed headers

--- shops/scrapers/amazon.py
+++ shops/scrapers/amazon.py
@@ -43,7 +43,7 @@ class AmazonScraper(BaseScraper):
             return cls.CACHE.get(f"{category}__{name}")
         except KeyError:
             pass
-        url = f"/s?k={name}"
+        url = f"/s?k={name.replace(' ', '+')}"
         url += f"&i={cls.NORMALIZED_CATEGORIES[category]}"
         url += "&ref=nb_sb_noss&url=search-alias%3Daps"
         async with aiohttp.ClientSession() as session:
@@ -56,13 +56,17 @@ class AmazonScraper(BaseScraper):
                     "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                     "accept-encoding": "gzip, deflate, br",
                     "accept-language": "en",
-                    "cache-control": "max-age=0",
+                    "cache-control": "no-cache",
+                    "ect": "4g",
+                    "downlink": "9",
+                    "pragma": "no-cache",
+                    "rtt": "150",
                     "sec-ch-ua": '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
                     "sec-ch-ua-mobile": '?0',
                     "sec-ch-ua-platform": '"Windows"',
                     "sec-fetch-dest": 'document',
                     "sec-fetch-mode": 'navigate',
-                    "sec-fetch-site": 'same-origin',
+                    "sec-fetch-site": 'none',
                     "sec-fetch-user": '?1',
                     "upgrade-insecure-requests": '1',
                     "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
